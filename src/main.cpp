ba834efbadbfca258b6b245ba3be784f35541b1c
Brooklyn-Dev <brooklynbaylisdev@gmail.com>
2025-06-01 13:05:42 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    // Init GLFW and OpenGL context
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    // Configure OpenGL context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ray-tracing", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL functions
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "Failed to initialise GLAD\n";
+        return -1;
+    }
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    glViewport(0, 0, 800, 600);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
